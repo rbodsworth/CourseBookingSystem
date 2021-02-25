5158d8ec95a78cab89918cc642c6d24eb399047d
Richard Bodsworth <richardbodsworth@gmail.com>
2021-02-25 13:43:15 +0000

    models complete

--- src/main/java/com/codeclan/example/CourseBookingSystem/Models/Booking.java
+++ src/main/java/com/codeclan/example/CourseBookingSystem/Models/Booking.java
@@ -1,4 +1,67 @@
 package com.codeclan.example.CourseBookingSystem.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private String date;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"bookings"})
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"bookings"})
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    public Booking(String date, Customer customer, Course course) {
+        this.date = date;
+        this.customer = customer;
+        this.course = course;
+    }
+
+    public Booking(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
